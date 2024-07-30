03f6730792e4e871717a3271dc1ba39c5a8ddd4f
Abdoabdo194 <abdljalilabdljalil205@gmail.com>
2024-07-30 23:32:29 +0100

    Update _anticall.js

--- plugins/_anticall.js
+++ plugins/_anticall.js
@@ -8,14 +8,19 @@ const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function (
 }, ms))
 
 export async function all(m) {
-	if (m.fromMe && m.isBaileys) return !0
-	let text;
-	let setting = global.db.data.settings[this.user.jid]
-	if(!setting.antiCall) return 
-	
-	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-		await this.reply(m.chat, 'kamu Di blockir oleh bot karena telah melanggar aturan bot\n\n*📮Dilarang menelepon Bot!*', null)
-		await delay(1000)
-		await this.updateBlockStatus(m.chat, "block")
-	}
+    if (m.fromMe && m.isBaileys) return !0
+    if (m.text) {
+        if (m.text === "") return
+        this.sendMessage(m.chat, { react: { text: "👍", key: m.key }})
+    }
+
+    let text;
+    let setting = global.db.data.settings[this.user.jid]
+    if(!setting.antiCall) return 
+
+    if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
+        await this.reply(m.chat, 'kamu Di blockir oleh bot karena telah melanggar aturan bot\n\n*ðŸ“®Dilarang menelepon Bot!*', null)
+        await delay(1000)
+        await this.updateBlockStatus(m.chat, "block")
+    }
 }
